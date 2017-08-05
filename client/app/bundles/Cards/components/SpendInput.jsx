57421e154d69eb0e6c94b6711ffb4c0ad1108774
Peter Glennon <pcrglennon@gmail.com>
2017-08-05 14:28:18 -0700

    Properly handle blank value in SpendInput, set to 0 on blur

--- client/app/bundles/Cards/components/SpendInput.jsx
+++ client/app/bundles/Cards/components/SpendInput.jsx
@@ -4,18 +4,26 @@ import PropTypes from 'prop-types';
 class SpendInput extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = { amount: this.props.amount };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   handleChange(event) {
     this.setState({ amount: event.target.value });
 
-    const parsedAmount = parseInt(event.target.value);
+    const parsedAmount = event.target.value === '' ? 0.0 : parseInt(event.target.value);
 
     if (!Number.isNaN(parsedAmount)) {
       this.props.updateAmount(parsedAmount);
+    }
+  }
+
+  handleBlur() {
+    if (this.state.amount === '') {
+      this.setState({ amount: 0.0 });
     }
   }
 
@@ -26,6 +34,7 @@ class SpendInput extends React.Component {
         <input
           value={this.state.amount}
           onChange={this.handleChange}
+          onBlur={this.handleBlur}
           type="number"
           step="0.01"
           min="0"
